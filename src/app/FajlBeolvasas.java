44fc9c3c5eef8b1324d11f23e7ea92a83a51c8d6
Huszár Fruzsina Anna (SZF_N_2025_2027) <huszar.fruzsina.anna2@diak.szamalk-szalezi.hu>
2026-05-08 14:20:37 +0200

    1 és 6 feladat kesz, de minden metodus void es csak kiirja a valaszt

--- src/app/FajlBeolvasas.java
+++ src/app/FajlBeolvasas.java
@@ -4,22 +4,41 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class FajlBeolvasas {
-    
-    
+ 
     private static List<Fuvar> fuvarlista = new ArrayList<>();
     
     public static void main(String[] args) throws IOException {
-        
-        Path path = Path.of("adatok.csv");
+        Path path = Path.of("fuvarok.csv");
         beolvas(path);
         
-        osszesKiir();
+        feladat1(); //kész
+        System.out.println("");
         
-        System.out.println("\n\n");
-        feladatok();
+        feladat2();
+        System.out.println("");
+        
+        feladat3();
+        System.out.println("");
+        
+        feladat4();
+        System.out.println("");
+        
+        feladat5();
+        System.out.println("");
+        
+        feladat6(); //kész
+        System.out.println("");
+        
+        feladat7();
+        System.out.println("");
+        
+        feladat8();
+        System.out.println("");
     }
 
     private static void beolvas(Path path) throws NumberFormatException, IOException {
@@ -37,7 +56,7 @@ public class FajlBeolvasas {
         }
     }
     
-        public static FizetesiMod szovegEnumm(String szoveg) {
+    private static FizetesiMod szovegEnumm(String szoveg) {
         return switch (szoveg) {
             case "kártya" -> FizetesiMod.KARTYA;
             case "készpénz" -> FizetesiMod.KESZPENZ;
@@ -53,15 +72,76 @@ public class FajlBeolvasas {
             System.out.println(fuvar);
         }
     }
+
+    private static void feladat1() {
+        System.out.println("1: Összes fuvar értéke?");
+        double eur = 0;
+        for(Fuvar fuvar : fuvarlista){
+            eur += fuvar.getOsszegEuro();
+        }
+        System.out.println("Összes fuvar értéke: " + eur);
+    }
     
-    public static void feladatok() {
-        int i = 0;
-        int N = fuvarlista.size();
-        while(i < N && true){
-            i++;
+    private static void feladat2() {
+        System.out.println("2: Legdrágább fuvar rendszáma?");
+        
+        String valasz = true ? "Igen" : "Nem";
+        System.out.println("Végig tudott futni/Összesre igaz? " + valasz);
+
+    }
+    
+    private static void feladat3() {
+        System.out.println("3: Legolcsóbb fuvar forintban?");
+        
+        String valasz = true ? "Igen" : "Nem";
+        System.out.println("Végig tudott futni/Összesre igaz? " + valasz);
+
+    }
+    
+    private static void feladat4() {
+        System.out.println("4: Hány Kártáys fizetés volt?");
+        
+        String valasz = true ? "Igen" : "Nem";
+        System.out.println("Végig tudott futni/Összesre igaz? " + valasz);
+
+    }
+    
+    private static void feladat5() {
+        System.out.println("5: Minden fzetési mód meghatározot?");
+        
+        String valasz = true ? "Igen" : "Nem";
+        System.out.println("Végig tudott futni/Összesre igaz? " + valasz);
+
+    }
+    
+    private static void feladat6() {
+        System.out.println("6: Hány db autó van a rendszerben?");
+        
+        Set<String> s = new HashSet<>();
+        
+        
+        for(Fuvar fuvar : fuvarlista){
+            s.add(fuvar.getRendszam());
         }
         
-        String valasz = i<=N ? "Igen" : "Nem";
-        System.out.println("Végig tudott futni/Összesre igaz? " + valasz);
+        System.out.println("Autók a rendszerben: " + s.size() + " db.");
+
     }
+    
+    private static void feladat7() {
+        System.out.println("7: Hányféle Fizetési mód van?");
+        
+        String valasz = true ? "Igen" : "Nem";
+        System.out.println("Végig tudott futni/Összesre igaz? " + valasz);
+
+    }
+    
+    private static void feladat8() {
+        System.out.println("8: Melyk autó mennyi fuvart teljesített?");
+        
+        String valasz = true ? "Igen" : "Nem";
+        System.out.println("Végig tudott futni/Összesre igaz? " + valasz);
+
+    }
+    
 }
